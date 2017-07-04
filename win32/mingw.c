792c7195b3795aaa377a5993197dfd0b5fcb5578
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-07-04 23:12:00 +0200

    mingw: also override chdir()
    This extends all the mingw_pathconv() magic to the chdir() function,
    allowing things like this in shell scripts:
    
    	cd /c/WINDOWS/system32
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- win32/mingw.c
+++ win32/mingw.c
@@ -1916,6 +1916,17 @@ ULONGLONG CompatGetTickCount64(void)
 }
 #endif
 
+#undef chdir
+int mingw_chdir(const char *path)
+{
+	wchar_t *wpath = mingw_pathconv(path);
+
+	if (!wpath)
+		return -1;
+
+	return _wchdir(wpath);
+}
+
 #if ENABLE_FEATURE_INSTALLER
 /*
  * Enumerate the names of all hard links to a file.  The first call
